6ec5b868e7c8182bb6c299a3cdac0b8fc697eff3
David Morán <david.morandz@gmail.com>
2013-12-18 09:30:34 +0100

    Commit inicial. Aplicacion creada sin Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coworkingon::Application.config.secret_key_base = '59fe303064dd1e807d8629b21c1f24bd3dbd279defe773aca2cde4c44efeaac896c3970c878b5633683aeff4d15be5a7abd715e5943e91255493098e33f5cb9c'
