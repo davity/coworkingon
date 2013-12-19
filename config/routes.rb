abe8fb9bb9d8304c0cf823eebbe8e555490ddea0
David Morán <david.morandz@gmail.com>
2013-12-19 14:58:54 +0100

    Implementada página principal de la web (home). Implementado un login y registro básico

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Coworkingon::Application.routes.draw do
 
+  get "home/index"
   devise_for :users
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
